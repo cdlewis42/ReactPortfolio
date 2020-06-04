f87af570378d0a9ed3789f27a571941964c8b4cc
Celeste Lewis <cdlewis42@gmail.com>
2020-06-04 16:03:52 -0400

    added some css

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import React,{Component} from "react"
 import Wrapper from "./components/Wrapper"
 import Carousel from "./components/Carousel"
 import Jumbotron from "./components/Jumbotron"
-
+import "./style.css"
 class App extends Component{
 render(){
     return (
         <Wrapper>
             {<Jumbotron></Jumbotron>}
+            <Carousel/>
             <Carousel/>
         </Wrapper>
             
--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Jumbotron from "react-bootstrap/Jumbotron"
+import "./style.css"
 
 class RenderJumbotron extends React.Component{
     
     render(){
-       return <Jumbotron>
+       return <Jumbotron className = "jumbotron">
         <h1>Celeste Lewis</h1>
         <h2>Software Developer</h2>    
         </Jumbotron>
